c4ffee2be0c6481bb65b393bc2c1919a752521a7
pardeep-mern <pardeep889@hotmail.com>
2020-06-14 10:26:51 +0530

    validation

--- src/components/validationProps/practice.js
+++ src/components/validationProps/practice.js
@@ -6,90 +6,53 @@ class Practice extends Component {
     this.state = {
       name: "pardeep",
       lastname: "Rajput",
-      err: false,
     };
     this.onsubmitForm = this.onsubmitForm.bind(this);
   }
 
-  handleInputNameChange(e) {
-    this.setState({
-      name: e.target.value,
-    });
-  }
+  // handleInputNameChange(e){
+  //     this.setState({
+  //         name: e.target.value
+  //     })
+  // }
 
-  handleLastNameChange(e) {
-    this.setState({
-      lastname: e.target.value,
-    });
-  }
+  // handleLastNameChange(e){
+  //     this.setState({
+  //         lastname: e.target.value
+  //     })
+  // }
   onsubmitForm(e) {
     e.preventDefault();
-    console.log(this.state);
-  }
-  formSubmitButton() {
-    if (this.state.name.length < 5) {
-      this.setState({ err: true });
-    }
+    console.log(e.target.name.value);
   }
 
-  //   renderFormInputs = (formValues) =>
-  //     formValues.map((item, i) => (
-  //       <span key={i}>
-  //         <input
-  //           type={item.type}
-  //           name={item.name}
-  //           defaultValue={item.value}
-  //         />
-  //         <br />
-  //       </span>
-  //     ));
+  renderFormInputs = (formValues) =>
+    formValues.map((item, i) => (
+      <span key={i}>
+        <input
+          type={item.type}
+          name={item.name}
+          defaultValue={item.value}
+        />
+        <br />
+      </span>
+    ));
 
   render() {
-    // const formValues = [
-    //     { type: "text", name: "name", value: "Pardeep" },
-    //     { type: "text", name: "lastname", value: "Rajput" },
-    //     { type: "text", name: "age", value: "28" }
-    // ];
-
-    // if (condition) {
-
-    // }
-
-    // if (condition) {
-
-    // } else {
-
-    // }
-    // {true && a}
-    // {true ? a: b}
-    const myErrorStyle = {
-      padding: "5px",
-      color: "red",
-    };
-
+    const formValues = [
+        { type: "email", name: "name", value: "Pardeep" },
+        { type: "text", name: "lastname", value: "Rajput" },
+        { type: "number", name: "age", value: "28" }
+    ];
     return (
       <div>
-        <form onSubmit={(e) => e.preventDefault()}>
-          {/* {this.renderFormInputs(formValues)} */}
-          <input
-            type="text"
-            onChange={(e) => this.setState({ name: e.target.value })}
-            value={this.state.name}
-          />
-          <br />
-          {this.state.err && (
-            <span style={myErrorStyle}>
-              There will be error inside above input
-            </span>
-          )}
-          <input
-            type="text"
-            onChange={(e) => this.setState({ lastname: e.target.value })}
-            value={this.state.lastname}
-          />
-          <br />
-          {/* <input type="submit" name="submit" /> */}
-          <button onClick={this.formSubmitButton.bind(this)}>Click me!</button>
+        <form onSubmit={(e) => this.onsubmitForm(e)}>
+          {this.renderFormInputs(formValues)}
+          {/* <input type="text" onChange={(e) => this.setState({name: e.target.value})} value={this.state.name} />
+                    <br/>
+                    <input type="text" onChange={(e) => this.setState({lastname: e.target.value})} value={this.state.lastname} />
+                    <br/> */}
+          <input type="submit" name="submit" />
         </form>
       </div>
     );
